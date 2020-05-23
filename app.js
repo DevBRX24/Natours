90e106f651cb2f5993d0eeb58130b11c90d91d62
DevBriX <45864249+DevBRX24@users.noreply.github.com>
2020-05-23 21:50:07 +0800

    Set up ESlint and Prettier in VSCODE

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-// Middleware function
-// Modify the incoming request data
 app.use(express.json());
-
-// 1.)) MIDDLEWARES
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  console.log('Hello from middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
-
-//ROUTE HANDLER/ API ENDPOINT /
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
-// 3.)) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
 module.exports = app;
