4d5698bfddda611efd24c76fa774e103a1bf8c4d
DevBriX <45864249+DevBRX24@users.noreply.github.com>
2020-05-23 21:10:48 +0800

    Created an  environment variable for development and production. Refactor the structure file fo server

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 // Middleware function
 // Modify the incoming request data
 app.use(express.json());
 
 // 1.)) MIDDLEWARES
-app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from middleware');
