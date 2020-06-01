d1086fdc08e1a90a4f75888c070341f808875c49
DevBriX <45864249+DevBRX24@users.noreply.github.com>
2020-06-01 08:43:37 +0800

    commit unhandledRejection function  for failed promises.It will throw an error message, error name and exit the process.implemented uncaugth Exception function for unexpected error

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 const monggoes = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -39,7 +45,15 @@ monggoes
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
