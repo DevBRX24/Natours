24f67fcc193abaa23d80f4a3ae8b8a018ce6507f
DevBriX <45864249+DevBRX24@users.noreply.github.com>
2020-06-04 20:02:10 +0800

    Deleting current user using deleteMe() function on userController. This will only set the active to false and will not show info on the client

--- models/userModel.js
+++ models/userModel.js
@@ -45,7 +45,12 @@ const userSchema = new monggoes.Schema({
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
-  passwordResetExpires: Date
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
 });
 
 userSchema.pre('save', function(next) {
@@ -64,6 +69,14 @@ userSchema.pre('save', async function(next) {
 
   // Delete passwordConfirm field
   this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.pre(/^find/, function(next) {
+  // find all the query that start with find
+  // this points to the current query
+  this.find({ active: { $ne: false } });
+
   next();
 });
 
