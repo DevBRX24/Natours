876a58903799117b87f408ed544fe00e0c4281b2
DevBriX <45864249+DevBRX24@users.noreply.github.com>
2020-06-10 18:43:31 +0800

    Import review and User data from json file on data folder

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -23,11 +25,15 @@ mongoose
 
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`));
 
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Succesfully Imported');
   } catch (error) {
     console.log(error);
@@ -39,6 +45,8 @@ const importData = async () => {
 const deletaData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data was deleted');
   } catch (error) {
     console.log(error);
@@ -51,3 +59,6 @@ if (process.argv[2] === '--import') {
 } else if (process.argv[2] === '--delete') {
   deletaData();
 }
+
+//Run this command for importing and deleting data on database
+// node ./dev-data/data/import-dev-data.js --delete or --import
