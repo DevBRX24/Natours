bfe427565611d4895fe7e842a77c3718409a50b2
DevBriX <45864249+DevBRX24@users.noreply.github.com>
2020-06-13 19:45:12 +0800

    Setting up Project Structure

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoute');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -62,10 +63,7 @@ app.use(
 );
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('base');
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
