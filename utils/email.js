62f8fa6d30f21c534268222d04e539328f116eb6
DevBriX <45864249+DevBRX24@users.noreply.github.com>
2020-06-23 16:56:54 +0800

    Credit card payment using stripe. Integrating Stripe into Back end

--- utils/email.js
+++ utils/email.js
@@ -12,7 +12,13 @@ module.exports = class Email {
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      return 1;
+      return nodeMailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
+        }
+      });
     }
 
     return nodeMailer.createTransport({
